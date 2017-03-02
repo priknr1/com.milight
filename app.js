1d4bbfc0a46ce8d3c06818118819002677ff324c
RobinBol <robinbolscher@gmail.com>
2017-03-02 14:55:21 +0100

    RGBW: realtime update light_mode on light_hue and light_temperature change

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
-"use strict";
+'use strict';
 
 const Log = require('homey-log').Log;
 
-var self = module.exports = {
-
-	init: function () {
-		console.log('com.milight is running...');
-	}
-
+module.exports.init = () => {
+	console.log(`${Homey.manifest.id} running...`);
 };
