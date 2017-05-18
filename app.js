57c45820fc51e55c4cc7f420f3b42b86ca491d80
RobinBol <robinbolscher@gmail.com>
2017-05-18 18:51:31 +0200

    Add night mode functionality for legacy bridge and iBox

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
 
 // TODO specific scene Flow card: toggleScene(sceneId)
 // TODO RGBWW driver
+// TODO remove TCP PING dependency, use discover bridges to check availability
+// TODO refactor to have a bridge only once in memory to prevent message overflow
+// TODO refactor legacy bridge to use node-milight-promise
+// TODO add flow card for night mode for bridge and new bulbs?
 
 const Log = require('homey-log').Log;
 
