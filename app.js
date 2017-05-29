c516ac3fa0b44f4eadec2c150b7bae452e5b3e2a
RobinBol <robinbolscher@gmail.com>
2017-05-29 20:41:49 +0200

    Rename Milight -> BridgeManager, fix a bug where discovery would happen even after bridge was already found, fix event listeners being bound too often

--- app.js
+++ app.js
@@ -3,14 +3,14 @@
 // TODO RGBWW driver
 // TODO refactor legacy bridge to use node-milight-promise
 // TODO multiple initDevice calls?
-// TODO after Unavailable/Available this.light is not properly working, might have to do with multiple devices all initDevice -> update IP -> update this.light and then the last update wins but is not updated on the other devices (keeping old session)
+// TODO after Unavailable/Available this.light is not properly working, might have to do with multiple devices all initDevice -> update IP -> update this.light and then the last update wins but is not updated on the other devices (keeping old session), also happened after app start
 // TODO test migration from old to new
 
 const Log = require('homey-log').Log;
 
-const MilightManager = require('./lib/milight/Milight.js');
+const BridgeManager = require('./lib/milight/BridgeManager.js');
 
 module.exports.init = () => {
 	console.log(`${Homey.manifest.id} running...`);
-	module.exports.BridgeManager = new MilightManager();
+	module.exports.BridgeManager = new BridgeManager();
 };
