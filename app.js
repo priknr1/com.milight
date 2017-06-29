0e59eeb23e93946f02915fb763fb27ab096f2182
RobinBol <robinbolscher@gmail.com>
2017-06-29 21:32:54 +0200

    Tested with legacy bridge, general improvements and fixes

--- app.js
+++ app.js
@@ -1,20 +1,7 @@
 'use strict';
 
-// TODO RGBWW driver
-// TODO refactor legacy bridge to use node-milight-promise
-// TODO multiple initDevice calls?
-// TODO test migration from old to new
-// TODO set specific effect mode should not be available for legacy rgbw
-// TODO online/offline
-// TODO emit onoff when using dim slider to turn on/off a bulb
-
-// TODO legacy rgbw hue is wrong
-
-// TODO RGBWW icons images
-
-// DONE checked RGB driver with legacy bridge AOK
-// DONE checked RGBW driver with lagacy bridge OK, except for wrong hue values or maybe rgb not sure
-
+// TODO bridge pairing wizard
+// TODO maybe SDKv2
 
 const Log = require('homey-log').Log;
 
