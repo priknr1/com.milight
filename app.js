87a7a5bc148a67130b83825986079a9e259b8f40
RobinBol <robinbolscher@gmail.com>
2017-06-30 17:06:26 +0200

    Refactoring for apps sdkv2 mostly finished

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 'use strict';
 
 // TODO bridge pairing wizard
-// TODO maybe SDKv2
+// TODO oibackoff like onInit() retry
+// TODO after updateIP() update zones in device object, they still refer to older zones with wrong session ids
 
+const Homey = require('homey');
 const Log = require('homey-log').Log;
 
 const BridgeManager = require('./lib/milight/BridgeManager.js');
 
-module.exports.init = () => {
-	console.log(`${Homey.manifest.id} running...`);
-	module.exports.BridgeManager = new BridgeManager();
-};
+class MilightApp extends Homey.HomeyApp {
+	onInit() {
+		this.log(`${this.id} running...`);
+		this._BridgeManager = new BridgeManager();
+	}
+
+	get BridgeManager() {
+		return this._BridgeManager;
+	}
+}
+
+module.exports = MilightApp;
